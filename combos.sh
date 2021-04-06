295407bee9e8afeaefe49cdeb80b550f34892818
mackcolwell <maco8719@colorado.edu>
2021-04-06 13:10:37 -0600

    rephrasing

--- combos.sh
+++ combos.sh
@@ -1,6 +1,6 @@
 #Pipe the output of the punnet squarer into this if all you want is the unique combos possible
 
-echo "Unique combinations possible:"
+echo "Unique results possible:"
 IFS= read line
 while IFS= read line; do
   echo ${line} | tr -d '|'
